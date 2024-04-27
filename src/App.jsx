758150f7c7f42f234db6232afb1ca9a337ec92f7
Joseph Konka <joseph.kakone@gmail.com>
2024-04-27 11:46:48 +0000

    Update Home

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "Joseph Konka",
+  title: "Microsoft Ambassador",
   email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  gitHub: "joekakone",
+  instagram: "joekakone",
+  linkedIn: "joseph-koami-konka",
+  medium: "joekakone",
+  twitter: "joekakone",
+  youTube: "algojungle",
 };
 
 const primaryColor = "#4E567E";
